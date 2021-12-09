13ca99e7280b784785b5677ac533d55ef4576ece
Brad Cornes <bradlc41@gmail.com>
2021-12-09 19:08:48 +0000

    Fix version dropdown z-index

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ export function Header({
 }) {
   return (
     <header
-      className="relative z-10 flex-none py-3 pl-5 pr-3 sm:pl-6 sm:pr-4 md:pr-3.5 lg:px-6 flex items-center space-x-4 antialiased"
+      className="relative z-20 flex-none py-3 pl-5 pr-3 sm:pl-6 sm:pr-4 md:pr-3.5 lg:px-6 flex items-center space-x-4 antialiased"
       style={{ fontFeatureSettings: '"cv02", "cv03", "cv04", "cv11"' }}
     >
       <div className="flex-auto flex items-center min-w-0 space-x-6">
