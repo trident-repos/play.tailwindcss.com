670ef206bfc7297129c26a10298c53ff08fafe17
Brad Cornes <hello@bradley.dev>
2022-09-23 13:39:13 +0100

    Upgrade `insiders` to `v0.0.0-insiders.bf44941` (#195)
    * [create-pull-request] automated change
    
    * Update `fast-glob` mock
    
    Co-authored-by: bradlc <bradlc@users.noreply.github.com>

--- src/modules/fast-glob.js
+++ src/modules/fast-glob.js
@@ -1,3 +1,12 @@
 module.exports = {
   sync: (patterns) => [].concat(patterns),
+  generateTasks: (patterns) => [
+    {
+      dynamic: false,
+      base: '.',
+      negative: [],
+      positive: [].concat(patterns),
+      patterns: [].concat(patterns),
+    },
+  ],
 }
